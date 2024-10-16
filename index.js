52edbfb9c51321068e191596eec8bf886bd22e39
choiyoungjin <82877643+cyjin0507@users.noreply.github.com>
2024-10-16 15:55:28 +0900

    한방단어 필터

--- index.js
+++ index.js
@@ -1,59 +1,55 @@
 let data = [];
 
-
 $.getJSON("./output.json", (jsonData) => {
     data = jsonData;
 });
 
-
 let value = "";
-let max = 6;
+let max = $("#word-length").val();
+let isCounter = true;
 
 const findValue = (array, searchValue) => {
     const isDescending = $("#desc").is(":checked");
-    return array.filter(item => item.Merged_Data.startsWith(searchValue) && item.Merged_Data.length <= max)
-        .sort((a, b) => isDescending ?
-            b.Merged_Data.length - a.Merged_Data.length :
-            a.Merged_Data.length - b.Merged_Data.length);
+    return array.filter(item => 
+        item.Merged_Data.startsWith(searchValue) && 
+        item.Merged_Data.length <= max
+    ).sort((a, b) => 
+        isDescending ? 
+        b.Merged_Data.length - a.Merged_Data.length : 
+        a.Merged_Data.length - b.Merged_Data.length
+    );
 };
 
 const draw = () => {
     const result = findValue(data, value);
     $("#list").html('');
-    result.forEach(x => {
-        $("#list").append(`
-            <p>${x.Merged_Data}</p>
-        `);
-    });
-}
 
+    if (isCounter) {
+        const specialItems = result.filter(x => x.Merged_Data.includes("한방"));
+        specialItems.forEach(x => {
+            $("#list").append(`<p style="color:red">${x.Merged_Data.replace("한방", "")}</p>`);
+        });
+    }
+
+    const regularItems = result.filter(x => !x.Merged_Data.includes("한방"));
+    regularItems.forEach(x => {
+        $("#list").append(`<p>${x.Merged_Data}</p>`);
+    });
+};
 
 $("#search").on('keyup', (e) => {
-    $("#list").html('');
     value = e.target.value;
-    if (value === "") {
-        return;
-    }
-    
+    if (value === "") return;
     draw();
 });
 
-
-$(".change-type").on('change', () => {
-    draw();
-});
-
-$(".word-length").on('change', (e) => {
-    const {target} = e;
+$(".change-type").on('change', draw);
+$("#word-length").on('change', (e) => {
     max = e.target.value;
+    draw();
+});
 
-    // if(type == "min" && min >= max) {
-    //     $("#min-length").val(min-1);
-    // } else if(type == "max" && min <= max) {
-    //     $("#max-length").val(max-1);
-    // }
-    draw()
-    
-    
-
-})
+$("#is-counter").on('change', (e) => {
+    isCounter = e.target.checked; // 체크박스의 checked 상태로 업데이트
+    draw();
+});
